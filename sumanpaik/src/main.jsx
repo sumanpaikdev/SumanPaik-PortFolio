f71f998b454d05582c8850b5fbd930b648dbf3c9
Suman Paik <sumanpaik.dev@gmail.com>
2023-01-17 20:09:55 +0530

    update portfolio

--- sumanpaik/src/main.jsx
+++ sumanpaik/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeContextProvider } from "./ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeContextProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ThemeContextProvider>
 );
